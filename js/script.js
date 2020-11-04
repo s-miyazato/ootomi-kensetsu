cbe17a2493786926a47c2e9e0306660a760792eb
宮里 紗矢香 <saya02storm3104@gmail.com>
2020-11-04 18:06:17 +0900

    header SP background-colorの表示設定の変更

--- js/script.js
+++ js/script.js
@@ -10,10 +10,12 @@ $(function() {
 
   //PC_header backdround-color、text-shadowの変更
   const headerVh = $('header').height();
+  const windowWidth = $(window).width();
 
   $(window).scroll(function() {
     const top = $(window).scrollTop();
-    if (headerVh < top) {
+
+    if (headerVh < top && windowWidth > 540) {
       $('.header__wrap').css('background-color', 'rgba(44,64,135,1)').css('text-shadow', 'none');
       } else {
       $('.header__wrap').css('background-color', 'rgba(44,64,135,0)').css('text-shadow', '0 3px 6px rgba(0, 0, 0, 0.5)');
